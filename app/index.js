a9a9c50fc14136496006eabf114c72e73876a5de
Fabian Bull <fabian.bull@bioinform.at>
2019-05-02 13:42:56 +0200

    Enable react in webpack
    Why:
    
    * React will be used in the UI.
    
    How:
    
    * Enable babel to transpile react files in the webpack build.

--- app/index.js
+++ app/index.js
@@ -1,1 +1,17 @@
-alert('Hello World!')
+import React from 'react'
+import { render } from 'react-dom'
+
+class App extends React.Component {
+    render() {
+        return (
+            <form>
+                Identifier: <input type="text" name="identifier"/><br/>
+                Sequence: <input type="text" name="sequence"/><br/>
+                <input type="submit" value="Submit"/>
+            </form>
+        );
+
+    }
+}
+
+render(<App/>, document.getElementById('app-container'))
