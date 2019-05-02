9cb0e7816acc6cb7ccdfbfb23d6a10b289397223
Fabian Bull <fabian.bull@bioinform.at>
2019-05-02 08:42:12 +0200

    Initial draft of backend
    Why:
    
    * The backend will serve as a man in the middle between frontend and the
      Elastic Search cluster.
    How:
    
    * This is a test of how to serve static html.

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "beagle"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
+
+// logging
+libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.2.3"
+
+// Webapp
+libraryDependencies += "com.typesafe.akka" %% "akka-http" % "10.0.10"
---
+++ src/main/scala/App.scala
@@ -0,0 +1,35 @@
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import org.slf4j.LoggerFactory
+
+import scala.io.StdIn
+
+object App {
+
+  private val Logger = LoggerFactory.getLogger(App.getClass.getName)
+
+  def main(args: Array[String]): Unit = {
+    Logger.info("Welcome to bIO - the search engine for biological sequences.")
+
+    implicit val system = ActorSystem("my-system")
+    implicit val materializer = ActorMaterializer()
+
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.dispatcher
+
+    val route =
+      path("" ) {
+        getFromFile("index.html") // uses implicit ContentTypeResolver
+      }
+
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done
+  }
+}
