c5503f4890153503930f2c780b782ea4429a54d3
Fabian Bull <fabian.bull@bioinform.at>
2019-05-25 13:45:53 +0200

    Add frontend Dockerfile
    Why:
    
    * We deploy with Docker
    
    How:
    
    * We use a multi-stage Dockerfile
      - The frontend is build in a Node container
      - The produced artifacts are copied into an Nginx container
    * Also, the Git hash now gets added to the index.html

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM node:8 as build-stage
+
+# Create app directory
+WORKDIR /usr/src/build
+
+# Install app dependencies
+# A wildcard is used to ensure both package.json AND package-lock.json are copied
+COPY package*.json ./
+RUN npm install
+
+# Copy sources
+COPY app ./app/
+
+# Copy Webpack config
+COPY webpack.config.js ./
+
+# Copy Babel config
+COPY .babelrc ./
+
+# Build the UI
+ARG commit_hash
+ENV COMMIT_HASH=$commit_hash
+RUN npm run build
+
+FROM nginx:1.15
+COPY --from=build-stage /usr/src/build/dist /usr/share/nginx/html
--- webpack.config.js
+++ webpack.config.js
@@ -4,33 +4,42 @@ const GitRevisionPlugin = require('git-revision-webpack-plugin');
 
 const gitRevisionPlugin = new GitRevisionPlugin();
 
-module.exports = {
-    entry: ['babel-polyfill', './app/index.js'],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/bundle.js'
-    },
-    devServer: {
-        contentBase: './dist'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './app/index.html',
-            templateParameters: {
-                COMMITHASH: gitRevisionPlugin.commithash()
-            }
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
+const config = (env, argv) => {
+
+    console.log('Environment: ', process.env)
+    console.log('Arguments: ', argv)
+
+    return {
+        entry: ['babel-polyfill', './app/index.js'],
+        mode: 'development',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'js/bundle.js'
+        },
+        devServer: {
+            contentBase: './dist'
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                filename: 'index.html',
+                template: './app/index.html',
+                templateParameters: {
+                    COMMITHASH: process.env.COMMIT_HASH || gitRevisionPlugin.commithash()
                 }
-            },
-        ]
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                    }
+                },
+            ]
+        }
     }
-};
+}
+
+module.exports = config
